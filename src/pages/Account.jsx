348129e1b544367f14bc8a5982b527c4278e6db7
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-10-19 10:04:15 +0700

    account design fix

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -13,16 +13,34 @@ export default function Account() {
 
   return (
     <main className='container mx-auto px-4 py-8'>
-      <h1 className='text-5xl font-bold mb-6'>My Templates</h1>
-      {loading && <p>Loading templates...</p>}
-      {error && <p>Error: {error}</p>}
-      {!loading && !error && (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
-          {templates.map((template) => (
-            <AccountCard key={template.id} template={template} />
-          ))}
+      <section className='px-4 py-8 sm:px-8 md:px-16 md:py-16'>
+        <div className='container-xl lg:container mx-auto flex flex-col lg:flex-row justify-between items-center gap-8'>
+          <div className='w-full lg:w-1/2'>
+            <h1 className='text-darkPurple text-3xl md:text-5xl font-heading font-semibold'>
+              My Account
+            </h1>
+          </div>
+          <a
+            href='/publish-template'
+            className='hover:bg-lightPurple hover:text-white bg-purple text-lightGray transition-colors duration-300 px-4 py-2 rounded-md'
+          >
+            Add Template
+          </a>
         </div>
-      )}
+      </section>
+      <section className='px-4 py-8 sm:px-8 md:px-16'>
+        <div className='container-xl lg:container mx-auto'>
+          {loading && <p>Loading templates...</p>}
+          {error && <p>Error: {error}</p>}
+          {!loading && !error && (
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
+              {templates.map((template) => (
+                <AccountCard key={template.id} template={template} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
     </main>
   );
 }
